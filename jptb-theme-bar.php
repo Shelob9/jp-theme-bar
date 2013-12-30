06da684489c152b3b35eb3dad6a4e25c578a8974
Josh Pollock <jpollock412@gmail.com>
2013-12-29 21:58:36 -0500

    update meta in main theme file

--- jptb-theme-bar.php
+++ jptb-theme-bar.php
@@ -1,14 +1,17 @@
 <?php
 /*
-Plugin Name: IJM Theme Switcher Bar
-Plugin URI: http://iainjmccallum.com/wordpress/live-demo-theme-bar/
-Description: Add a theme switcher / theme demo bar to your site. Allows users to switch the theme they see on your site.
-Version: 2.0
-Author: Iain J McCallum
-Author URI: http://www.iainjmccallum.com/
-License: GPL
+Plugin Name: JP Theme Switcher Bar
+Plugin URI:
+Description: Adds a theme switcher / theme demo bar to the bottom of your site to allow users to switch the theme they see on your site.
+Version: 0.0.1
+Author: Josh Pollock
+Author URI: http://www.JoshPress.net
+License: GPLv2+
 */
-
+/**
+ * This plugin is based on IJM Theme Switcher Bar  v2.0 (http://iainjmccallum.com/wordpress/live-demo-theme-bar/)
+ *  by Iain J McCallum (http://www.iainjmccallum.com/)
+ */
 
 //Include the front-end
 require_once( 'jptb-frontend.php' );
