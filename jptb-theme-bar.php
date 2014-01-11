d3a147aa8793aa2e8d52b3b698a7e30b8e2a8420
Josh Pollock <jpollock412@gmail.com>
2014-01-11 02:20:33 -0500

    version # 0.0.2

--- jptb-theme-bar.php
+++ jptb-theme-bar.php
@@ -3,7 +3,7 @@
 Plugin Name: JP Theme Switcher Bar
 Plugin URI: https://github.com/shelob9/jp-theme-bar
 Description: Adds a theme switcher / theme demo bar to the bottom of your site to allow users to switch the theme they see on your site.
-Version: 0.0.1
+Version: 0.0.2
 Author: Josh Pollock
 Author URI: http://www.JoshPress.net
 License: GPLv2+
