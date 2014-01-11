f3222b3765bd5c2280930b20ee7c5da2ad0dfe67
Josh Pollock <jpollock412@gmail.com>
2014-01-10 23:47:33 -0500

    inline docs and filters for front-end

--- jptb-frontend.php
+++ jptb-frontend.php
@@ -14,59 +14,117 @@ class frontend {
         wp_enqueue_script( 'jptb-js', plugin_dir_url( __FILE__ ).'jptb-frontend.js', array('jquery'), null, true );
    }
 
-function css () { ?>
-    <style>
-            #jptb-theme-bar {
-                bottom:0px;
-                position:fixed;
-                left:0px;
-                width:100%;
-                height:28px;
-                background-color:<?php echo get_option('jptb_bg_colour') ?>;
-                color:<?php echo get_option('jptb_text_colour'); ?>;
-                text-shadow:none;
-                z-index:9999;
-            }
+    /**
+     * Create an array of colours from the settings.
+     *
+     * @package jptb
+     * @since 0.0.2
+     *
+     * @return array $colours All the colours we need for styles
+     */
+    function colours() {
+        $colours = array(
+            'bg_colour'         => get_option('jptb_bg_colour'),
+            'text_colour'       => get_option('jptb_text_colour'),
+            'label_bg_colour'   => get_option('jptb_label_bg_colour'),
+            'label_text_colour' => get_option('jptb_label_text_colour')
+        );
+        /**
+         * Filter to overide colours for the switcher bar
+         *
+         * @param   array   $colours    An array of colours
+         *
+         * @since 0.0.1
+         */
+        $colours = apply_filters( 'jptb_bar_colours', $colours );
+        return $colours;
+    }
 
-            #jptb-theme-bar ul, #jptb-theme-bar p {
-                margin:0;
-                padding:0;
-            }
+    /**
+     * Create stylesheet for the actual switcher bar
+     *
+     * @package jptb
+     * @since 0.0.2
+     *
+     * @return  string  $style  The styles for the actual switcher bar.
+     */
+    function style() {
+        $colours = $this->colours();
+        $style = "
+                #jptb-theme-bar {
+                    bottom:0px;
+                    position:fixed;
+                    left:0px;
+                    width:100%;
+                    height:28px;
+                    background-color: ".$colours[ 'bg_colour'].";
+                    color: ".$colours[ 'text_colour' ].";
+                    text-shadow:none;
+                    z-index:9999;
+                }
 
-            #jptb-theme-bar ul li {
-                float:left;
-                list-style:none;
-                margin-left:10px;
-                content:"" !important;
-                content:none !important;
-            }
+                #jptb-theme-bar ul, #jptb-theme-bar p {
+                    margin:0;
+                    padding:0;
+                }
 
-            #jptb-theme-bar ul li:before {
-                content:"" !important;
-                content:none !important;
-            }
+                #jptb-theme-bar ul li {
+                    float:left;
+                    list-style:none;
+                    margin-left:10px;
+                    content:'' !important;
+                    content:none !important;
+                }
 
-            #jptb-theme-bar a, #jptb-theme-bar p {
-                color:<?php echo get_option('jptb_text_colour'); ?>;
-                text-decoration:none;
-                line-height:15px;
-                font: normal 13px/28px sans-serif;
-            }
+                #jptb-theme-bar ul li:before {
+                    content:'' !important;
+                    content:none !important;
+                }
 
-            #jptb-theme-bar a:hover {
-                color:<?php echo get_option('jptb_text_colour'); ?>;
-                text-decoration:underline;
-            }
+                #jptb-theme-bar a, #jptb-theme-bar p {
+                    color: ".$colours[ 'text_colour' ].";
+                    text-decoration:none;
+                    line-height:15px;
+                    font: normal 13px/28px sans-serif;
+                }
 
-            p#jptb_label {
-                background-color:<?php echo get_option('jptb_label_bg_colour') ?>;
-                color:<?php echo get_option('jptb_label_text_colour'); ?>;
-                padding: 0 4px;
-                left: 0px;
-                position: absolute;
-            }
-        </style>
-<?php
+                #jptb-theme-bar a:hover {
+                    color: ".$colours[ 'text_colour' ].";
+                    text-decoration:underline;
+                }
+
+                p#jptb_label {
+                    background-color: ".$colours[ 'label_bg_colour' ].";
+                    color: ".$colours[ 'label_text_colour' ].";
+                    padding: 0 4px;
+                    left: 0px;
+                    position: absolute;
+                }
+            ";
+        /**
+         * Filter the styles.
+         *
+         * Use this filter to completely overide the styles being used.
+         *
+         * @param   $string $style A style sheet (with no <style></style>
+         */
+        $style = apply_filters( 'jptb_bar_style', $style );
+        return $style;
+    }
+
+    /**
+     * Output the stylesheet
+     *
+     * @package jptb
+     * @since 0.0.1
+     */
+    function css() {
+        //create the stylesheet, with tags, using this::style()
+        $css = '<style>';
+        $css .= $this->style();
+        $css .= '</style>';
+        //make it so
+        echo $css;
     }
 
     /**
